16f9d7999c8caabe751eec4569987bd7c60362ff
wWqaq <3248604531@qq.com>
2025-07-21 14:25:21 +0800

    login初步创建

---
+++ src/test/java/com/ww/loginmvp/LoginMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ww.loginmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
